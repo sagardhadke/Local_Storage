be1cc2e984b9362a2eb908b4bd719080a909fdbd
Sagar Dhadke <peaceunique2003@gmail.com>
2024-08-31 17:20:35 +0530

    Added Login and Sign Up

---
+++ lib/Login.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyLogin extends StatefulWidget {
+  const MyLogin({super.key});
+
+  @override
+  State<MyLogin> createState() => _MyLoginState();
+}
+
+class _MyLoginState extends State<MyLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:local_storage_pract/Login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,15 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        title: 'Local Storage Shared Preferences',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: Scaffold(
-          body: Column(
-            children: [],
-          ),
-        ));
+        home: MyLogin());
   }
 }
